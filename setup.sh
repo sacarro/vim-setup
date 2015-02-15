e45a715941231d5a439bf0ee69303b37136d4e37
sacarro <samuel.carroll@inficon.com>
2015-02-15 10:55:38 -0500

    Setup file to for common plugins I use with VIM when developing javascript

---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+CWD=`pwd`
+hasVim=`which vim 1>/dev/null 2>/dev/null`
+
+if [ $? -ne 0 ]; then
+    sudo apt-get install vim
+fi
+
+# Get the .vimrc file going
+if [ -f ~/.vimrc ]; then
+    cp ~/.vimrc ~/.vimrc_BACKUP 
+fi 
+echo "" > ~/.vimrc
+
+# Now start building the plugins
+
+################# Pathogen ########################
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "execute pathogen#infect()" >> ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "filetype plugin indent on" >> ~/.vimrc
+
+################# Sensible ########################
+cd ~/.vim/bundle && \
+git clone git://github.com/tpope/vim-sensible.git
+
+################# Solarized ########################
+cd ~/.vim/bundle && \
+git clone git://github.com/altercation/vim-colors-solarized.git
+echo "syntax enable" >> ~/.vimrc
+echo "set background=dark" >> ~/.vimrc
+echo "colorscheme solarized" >> ~/.vimrc
+
+################# Surround ########################
+cd ~/.vim/bundle && \
+git clone git://github.com/tpope/vim-surround.git
+
+################# JavaScript ########################
+cd ~/.vim/bundle && \
+git clone https://github.com/pangloss/vim-javascript.git
+echo "javascript_enable_domhtmlcss=1"
+
+################# Angular ########################
+cd ~/.vim/bundle && \
+git clone git://github.com/burnettk/vim-angular.git
+
+################# JavaScript Libraries ########################
+cd ~/.vim/bundle && \
+git clone https://github.com/othree/javascript-libraries-syntax.vim.git
+echo "autocmd BufReadPre *.js let b:javascript_lib_use_angularjs = 1"  >> ~/.vimrc
+echo "autocmd BufReadPre *.js let b:javascript_lib_use_backbone = 0"  >> ~/.vimrc
+
+################# Syntastic ########################
+cd ~/.vim/bundle && \
+git clone https://github.com/scrooloose/syntastic.git
+echo "set statusline+=%#warningmsg#" >> ~/.vimrc
+echo "set statusline+=%{SyntasticStatuslineFlag()}" >> ~/.vimrc
+echo "set statusline+=%*" >> ~/.vimrc
+
+echo "let g:syntastic_always_populate_loc_list = 1" >> ~/.vimrc
+echo "let g:syntastic_auto_loc_list = 1" >> ~/.vimrc
+echo "let g:syntastic_check_on_open = 1" >> ~/.vimrc
+echo "let g:syntastic_check_on_wq = 0" >> ~/.vimrc
+
+################# JS Beautify ########################
+cd ~/.vim/bundle && \
+git clone https://github.com/maksimr/vim-jsbeautify.git && \
+cd vim-jsbeautify && \
+git submodule update --init --recursive
+
+
+
